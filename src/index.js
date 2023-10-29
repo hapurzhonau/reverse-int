cb058666c4612769102a7027c53f643723dd81f5
Daniyar Hapurzhonau <hapurzhonau@gmail.com>
2023-10-30 00:38:53 +0300

    feat: complete task (Mon, Oct 30, 2023 12:38:53 AM)

--- src/index.js
+++ src/index.js
@@ -1,6 +1,3 @@
 module.exports = function reverse(n) {
-    if (n > 0) {
-        return n.toString().split('').reverse().join('');
-    }
-    return '-' + (n * (-1)).toString().split('').reverse().join('');
+    return Math.abs(n).toString().split('').reverse().join('');
 }
